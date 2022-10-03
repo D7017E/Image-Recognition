36e78be090d12ad5e1e553084d6506c6f8af835a
Dennis Trollsfjord <dennist1@live.se>
2022-10-03 10:28:42 +0200

    added download of dataset

--- src/dataset.py
+++ src/dataset.py
@@ -4,10 +4,17 @@ import tensorflow_datasets as tfds
 class Dataset():
     
     def __init__(self) -> None:
-        self.sample = self._load_sample()
+        self.train_sample, self.test_sample = self._load_sample()
         print(f"")
 
     def _load_sample(self) -> tf.data.Dataset:
-        sample = tfds.load("rock_paper_scissors", split="train", shuffle_files=True, as_supervised=True, try_gcs=True)
-        assert isinstance(sample, tf.data.Dataset)
-        return sample
+        train_sample, test_sample = tfds.load(
+            "rock_paper_scissors", 
+            split=["train", "test[:30%]"], 
+            data_dir="dataset", 
+            shuffle_files=True, 
+            as_supervised=True, 
+            try_gcs=True)
+        assert isinstance(train_sample, tf.data.Dataset)
+        assert isinstance(test_sample, tf.data.Dataset)
+        return train_sample, test_sample
