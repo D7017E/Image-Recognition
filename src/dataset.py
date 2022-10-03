d9fe0102dcc76455fe750ca1851fdb6176a5715b
Dennis Trollsfjord <dennist1@live.se>
2022-10-03 10:16:35 +0200

    import as supervised

--- src/dataset.py
+++ src/dataset.py
@@ -5,8 +5,9 @@ class Dataset():
     
     def __init__(self) -> None:
         self.sample = self._load_sample()
+        print(f"")
 
     def _load_sample(self) -> tf.data.Dataset:
-        sample = tfds.load("rock_paper_scissors", split="train", try_gcs=True)
-        assert isinstance(self.sample, tf.data.Dataset)
+        sample = tfds.load("rock_paper_scissors", split="train", shuffle_files=True, as_supervised=True, try_gcs=True)
+        assert isinstance(sample, tf.data.Dataset)
         return sample
