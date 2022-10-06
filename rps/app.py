1c3070a03126fb20c261cf57c0acd5008801c5e2
Dennis Trollsfjord <dennist1@live.se>
2022-10-06 15:32:59 +0200

    example

---
+++ rps/app.py
@@ -0,0 +1,22 @@
+import qi
+import helper
+import mylib
+
+class RPS:
+
+    def echo(self, message):
+        return message
+
+    def do_something(self):
+        pass
+
+def main():
+    app = qi.Application()
+    app.start()
+    session = app.session
+    my_service = RPS()
+    session.registerService("RPS", my_service)
+    app.run()
+
+if __name__ == "__main__":
+    main()
