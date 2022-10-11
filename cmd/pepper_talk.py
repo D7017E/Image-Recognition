3e8891a6018ecd95ab9b19a5d8f51a11cf2a0814
Dennis Trollsfjord <dennist1@live.se>
2022-10-11 13:29:34 +0200

    added pepper test

---
+++ cmd/pepper_talk.py
@@ -0,0 +1,28 @@
+import sys, getopt
+from naoqi import ALProxy
+
+# https://www.tutorialspoint.com/python/python_command_line_arguments.htm
+
+def main(argv):
+    message = 'Hello World!'
+    try: 
+        opts, args = getopt.getopt(argv, "m:")
+    except getopt.GetoptError:
+        print("pepper_talk.py -m <message>")
+        sys.exit(2)
+    
+    if len(opts) >= 1:
+        print(opts)
+        message = ''
+        opt, arg = opts[0] 
+        if opt in ("-m", "--message"):
+            message += arg
+        for arg in args:
+            message += arg
+
+    tts = ALProxy("ALTextToSpeech", "130.240.238.32", 9559)
+    tts.say(message)
+
+if __name__ == "__main__": 
+    print(sys.argv)
+    main(sys.argv[1:])
