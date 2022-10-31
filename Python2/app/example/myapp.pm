750d19988679ec2ffef1728d8cc91e61bcaa327a
Dennis Trollsfjord <dennist1@live.se>
2022-10-31 14:39:38 +0100

    application

---
+++ Python2/app/example/myapp.pm
@@ -0,0 +1,12 @@
+<Package name="myapp" format_version="4">
+  <Manifest src="manifest.xml" />
+  <!--
+  <Resources>
+    <File src="path/to/some/data" />
+  </Resources>
+  -->
+
+  <!-- if written in Python -->
+  <qipython name="myservice" />
+
+</Package>
--- lint.sh
+++
@@ -1,1 +0,0 @@
-python -m pipenv run pylint src/
